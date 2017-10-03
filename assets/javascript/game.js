4b6b7cb4855fb763e48967fc46253346cbb5adc9
Matthew Lloyd <MLloyd09@gmail.com>
2017-10-03 16:52:33 -0500

    Added all the appropriate files

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+console.log("Are you reading me??")
+console.log("The link has been made")
+
+    document.getElementById("startup-text").addEventListener("click", function(){
+
+            document.getElementById("box").style.color = "green";
+
+        });
